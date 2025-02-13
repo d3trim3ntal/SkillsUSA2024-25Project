72569d440fa4073be88ed6962af13e201c418f1b
SmallBowl <awsuhime@gmail.com>
2025-02-13 15:07:25 -0800

    Marker Selection and started pickup

--- Skilss25/Assets/SOULScripts/RobotRework.cs
+++ Skilss25/Assets/SOULScripts/RobotRework.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class RobotRework : MonoBehaviour
 {
@@ -9,17 +10,26 @@ public class RobotRework : MonoBehaviour
     private NavMeshAgent agent;
 
     List<int> assignedActions = new List<int>();
-    //1 for movement
+    //currentaction is used when assigning actions, while runningaction is used when the robot is performing the actions
     private int currentAction;
     private int runningAction;
     public int maxActions;
     public float markerRange = 10;
 
+    private GameObject closest;
     private Dictionary<int, GameObject> assignedMarkers = new Dictionary<int, GameObject>();
-    //State bools
+
+    [Header("UI")]
+    public TextMeshProUGUI fixText;
+    public TextMeshProUGUI selectText;
+    public TextMeshProUGUI sequenceText;
+    public TextMeshProUGUI actionsText;
+
+    [Header("State Bools")]
     public bool running;
     public bool beingFixed;
     public bool moving;
+    public bool pickingUp;
     private bool selecting; //Selecting location
     void Start()
     {
@@ -36,6 +46,9 @@ public class RobotRework : MonoBehaviour
             //Cancel marker select
             if (selecting && Input.GetMouseButtonDown(1))
             {
+                fixText.gameObject.SetActive(true);
+                selectText.gameObject.SetActive(false);
+
                 selecting = false;
                 foreach (Marker i in markers)
                 {
@@ -54,15 +67,21 @@ public class RobotRework : MonoBehaviour
                 }
                 else
                 {
+                    updateSequenceText();
                     beingFixed = true;
+                    fixText.gameObject.SetActive(true);
                 }
             }
 
-            if (beingFixed && !selecting)
+            if (beingFixed && !selecting && assignedActions.Count != maxActions)
+                
             {
                 //Temporary key for starting location selection
                 if (Input.GetKeyDown(KeyCode.M))
                 {
+                    fixText.gameObject.SetActive(false);
+                    selectText.gameObject.SetActive(true);
+
                     selecting = true;
                     foreach (Marker i in markers)
                     {
@@ -72,6 +91,12 @@ public class RobotRework : MonoBehaviour
                         }
                     }
                     Cursor.lockState = CursorLockMode.None;
+                }
+                else if (Input.GetKeyDown(KeyCode.Semicolon))
+                {
+                    assignedActions.Add(2);
+                    currentAction++;
+                    updateSequenceText();
                 }
             }
         }
@@ -86,6 +111,12 @@ public class RobotRework : MonoBehaviour
 
                 
             }
+            if (pickingUp && Vector3.Distance(closest.transform.position, transform.position) < 3f)
+            {
+                closest = null;
+                pickingUp = false;
+                continueActions();
+            }
         }
         
     }
@@ -93,6 +124,8 @@ public class RobotRework : MonoBehaviour
 
     public void assignMarker(GameObject marker)
     {
+        selectText.gameObject.SetActive(false);
+        fixText.gameObject.SetActive(true);
         Cursor.lockState = CursorLockMode.Locked;
 
         selecting = false;
@@ -103,16 +136,16 @@ public class RobotRework : MonoBehaviour
         assignedMarkers[currentAction] = marker;
         assignedActions.Add(1);
         currentAction++;
-        if (currentAction == maxActions--)
-        {
-            initiateActions();
-        }
+        updateSequenceText();
+
+        
 
 
     }
 
     private void initiateActions()
     {
+        fixText.gameObject.SetActive(false);
         beingFixed = false;
         running = true;
         if (currentAction > 0)
@@ -121,6 +154,31 @@ public class RobotRework : MonoBehaviour
             {
                 moving = true;
                 agent.SetDestination(assignedMarkers[runningAction].transform.position);
+            }
+            else if (assignedActions[runningAction] == 2)
+            {
+                GameObject[] pickups = GameObject.FindGameObjectsWithTag("Pickup");
+                float closestDistance = 25f;
+                closest = null;
+                foreach (GameObject pickup in pickups)
+                {
+                    if (Vector3.Distance(transform.position, pickup.transform.position) < closestDistance)
+                    {
+                        closest = pickup;
+                        closestDistance = Vector3.Distance(transform.position, pickup.transform.position);
+                    }
+                }
+
+                if (closest != null)
+                {
+                    pickingUp = true;
+                    agent.SetDestination(closest.transform.position);
+                }
+                else
+                {
+                    continueActions();
+                }
+
             }
         }
         else
@@ -141,6 +199,32 @@ public class RobotRework : MonoBehaviour
                     moving = true;
                     agent.SetDestination(assignedMarkers[runningAction].transform.position);
                 }
+                //Pickup
+                else if (assignedActions[runningAction] == 2)
+                {
+                    GameObject[] pickups = GameObject.FindGameObjectsWithTag("Pickup");
+                    float closestDistance = 25f;
+                    closest = null;
+                    foreach (GameObject pickup in pickups)
+                    {
+                        if (Vector3.Distance(transform.position, pickup.transform.position) < closestDistance)
+                        {
+                            closest = pickup;
+                            closestDistance = Vector3.Distance(transform.position, pickup.transform.position);
+                        }
+                    }
+
+                    if (closest != null)
+                    {
+                        pickingUp = true;
+                        agent.SetDestination(closest.transform.position);
+                    }
+                    else
+                    {
+                        continueActions();
+                    }
+
+                }
             }
         }
         else
@@ -155,6 +239,30 @@ public class RobotRework : MonoBehaviour
         assignedActions.Clear();
         runningAction = 0;
         currentAction = 0;
+        updateSequenceText();
         running = false;
+
     }
+
+    private void updateSequenceText()
+    {
+        string guy = "";
+
+        foreach (int i in assignedActions)
+        {
+            if (i == 1)
+            {
+                guy += "Move ";
+            }
+            if (i == 2)
+            {
+                guy += "Pickup ";
+            }
+        }
+        sequenceText.text = guy;
+
+        actionsText.text = (maxActions - assignedActions.Count) + " actions left.";
+    }
+
+    
 }
